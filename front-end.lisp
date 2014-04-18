ec47c378998249cca902546a6f9f78b211756ff6
inaimathi <inaimathi@self>
2014-04-18 15:09:11 -0400

    -Cells now collect their results too -Warnings, conditions and such are properly highlighted

--- front-end.lisp
+++ front-end.lisp
@@ -497,16 +497,28 @@
 .cm-s-default .cm-hr {color: #999;}
 .cm-s-default .cm-link {color: #00c;}
 .cm-s-default .cm-error {color: #f00;}
+
+.result { border: 1px solid #ccc; background-color: #fff; list-style-type: none; margin: 0px; margin-top: 5px; padding: 0px; }
+.stdout { margin: 0px; padding: 5px; color: #8b2252; background-color: #efefef; }
+.result li { padding: 5px; }
+.result .type { color: #228b22; }
+.warnings .condition-contents { background-color: #fc6; color: #c60; border: 1px solid #c60; padding: 5px; margin: 5px 0px; }
+.result .error { background-color: #fdd; color: #933; }
+.condition-contents { list-style-type: none; margin: 0px; padding: 0px; }
+.condition-contents .condition-type { font-weight: bolder; }
+.condition-contents .condition-property { font-style: oblique; }
+.condition-contents .condition-property .label { display: inline-block; margin-right: 5px; font-size: small; }
 -->")
 		   (:h1 (notebook-name *notebook*)))
 		    (join 
 		     (map (lambda (cell) 
-			    (case (@ cell 'cell-type)
+			    (case (@ cell cell-type)
 			      ("commonLisp" 
 			       (let ((node (chain document (create-element "pre"))))
 				 (chain node (set-attribute :class "cm-s-default"))
 				 (chain -code-mirror (run-mode (@ cell contents) "commonlisp" node))
-				 (@ node outer-h-t-m-l)))
+				 (+ (@ node outer-h-t-m-l)
+				    (@ (by-cell-id (@ cell id) ".cell-value" "pre") outer-h-t-m-l))))
 			      ("clWho"
 			       (@ cell 'value 0 'values 0 'value))
 			      (t
