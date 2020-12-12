cfb911261fb070f867619f15bfd17ea7a1a0882e
inaimathi <leo.zovic@gmail.com>
2020-12-11 21:34:34 -0500

    Stub out failing tests

--- test/cl-notebook.lisp
+++ test/cl-notebook.lisp
@@ -2,11 +2,12 @@
 
 (tests
  (is (+ 2 3) 5 "Addition works")
- (is (+ 2 3) 6 "Intentionally fails")
+ ;; (is (+ 2 3) 6 "Intentionally fails")
 
  (for-all ((a a-number) (b a-number))
 	  (is= (+ a b) (+ b a))
 	  "Addition is commutative")
- (for-all ((a a-number) (b a-number))
-	  (is= (- a b) (- b a))
-	  "Subtraction is not, so this should fail"))
+ ;; (for-all ((a a-number) (b a-number))
+ ;; 	  (is= (- a b) (- b a))
+ ;; 	  "Subtraction is not, so this should fail")
+ )
